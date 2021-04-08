4bc677c0c8127a088da74dfac4e80b9d15dbe2c1
Guillermoç <guillermodean@hotmail.com>
2021-04-08 19:56:35 +0200

    use cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express= require('express');
 const app = express();
+const cors= require ('cors');
 
 const database = require('./database')
-
+app.use (cors());
 app.use (express.json());
 
 app.use('/api',require('./routes/index'))
