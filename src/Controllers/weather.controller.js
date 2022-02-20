b293f82e6f5dc77f8009cf4d3bb06716d20c68cc
Guillermoç <guillermodean@hotmail.com>
2022-02-20 18:19:48 +0100

    weather call done

--- src/Controllers/weather.controller.js
+++ src/Controllers/weather.controller.js
@@ -5,8 +5,7 @@ const weatherCtrl = {}
 
 // Get all tasks
 weatherCtrl.getWeather = async (req, res) => {
-    request("https://api.openweathermap.org/data/2.5/weather?q=Pamplona,es&appid=a7ce59d0350ce5cab2d235463756ea21", (err, response, body) => {
-        console.log(body);
+    request("https://api.openweathermap.org/data/2.5/weather?q=Pamplona,es&appid=a7ce59d0350ce5cab2d235463756ea21&units=metric", (err, response, body) => {
         res.status(200).json(JSON.parse(body));
     })
 }
