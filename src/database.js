d7fdf4161822ce16d5c803a8829087ad15835e82
Guillermo Deán <guillermodean@hotmail.com>
2022-11-29 09:17:55 +0100

    env var

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+const ENTORNO = require("../config.json")
 
 
-mongoose.connect('mongodb+srv://Guillermo:Ragnarson11@cluster0.tqdy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://'+ENTORNO.PRODUCCION.bbdduser+':'+ENTORNO.PRODUCCION.bbddpsw+'@cluster0.tqdy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify: false,
